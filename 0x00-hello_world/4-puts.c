fbbd209d69904f1b5907414680ee96e6d9d6555f
festusdisi12 <princedisifestus18@gmail.com>
2022-12-08 10:35:20 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * FILE: 4puts.c
+ * Auth: Brennan D Barabam <375@holbertonschool.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - printf "programming is like building a multilingual puzzle, followed by a new line"
+ *
+ *
+ * Return: Always o
+ */
+int main(void)
+{
+	puts ("\" programming is like building a multiligual puzzle");
+
+	return (0);
+}
